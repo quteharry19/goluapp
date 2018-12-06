0d6f5aae4d2bb173d2d766c111ee54dd105ce834
quteharry19 <quteharry19@hotmail.com>
2018-12-06 14:45:52 +0530

    requirement complete

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
 const sqlite3 = require('sqlite3');
+const path = require('path');
 const bodyparser = require('body-parser');
 const app = express();
+
+const db = new sqlite3.Database("/football/football.db");
